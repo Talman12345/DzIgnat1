6aab70d0cb2aadae5a051c60c421000d7d5316d0
Anatoliy11111 <canatolij@list.ru>
2022-01-08 15:20:37 +0300

    delete render, refactor state

--- happyHacking/Homeworks/projectdop/src/p2-homeworks/h5/Header.tsx
+++ happyHacking/Homeworks/projectdop/src/p2-homeworks/h5/Header.tsx
@@ -3,18 +3,23 @@ import {NavLink} from 'react-router-dom'
 import {PATH} from "./Routes";
 import s from './Hader.module.css'
 
+
 function Header() {
 
     const [btnBurger, setBtnBurger] = useState<boolean>(false)
-
+    const classColor = ({isActive}: any) => isActive ? s.active : s.passive
 
     return (
         <div>
             <div>
-                <button className={s.button} onClick={()=>{setBtnBurger(!btnBurger)}}>Level</button>
-            {btnBurger &&   <NavLink className={s.passive} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>}
-            {btnBurger &&    <NavLink className={s.passive} to={PATH.JUNIOR}>Junior</NavLink>}
-            {btnBurger &&       <NavLink className={s.passive} to={PATH.JUNIOR_PLUS}>Junior++</NavLink>}
+                <button className={s.button} onClick={() => {
+                    setBtnBurger(!btnBurger)
+                }}>Level
+                </button>
+                {btnBurger && <> <NavLink className={classColor} to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+                    <NavLink className={classColor} to={PATH.JUNIOR}>Junior</NavLink>
+                    <NavLink className={classColor} to={PATH.JUNIOR_PLUS}>Junior++</NavLink>
+                </>}
             </div>
         </div>
     )
